a0862764cec5f0b6cfa38354514b51b789cf8d7c
George Spake <gpspake@gmail.com>
2017-02-04 15:46:59 -0600

    render choose winner button

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const App = React.createClass({
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button>Choose Winner</button>
       </div>
     );
   }
