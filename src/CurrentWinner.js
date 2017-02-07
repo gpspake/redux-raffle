0d3c2771bb2228ad86869f5b541d8a2d575433b3
George Spake <gpspake@gmail.com>
2017-02-07 10:40:11 -0600

    add current winner click handlers

--- src/CurrentWinner.js
+++ src/CurrentWinner.js
@@ -1,14 +1,22 @@
 import React from 'react';
 
 const CurrentWinner = React.createClass({
+  handleNotHere(){
+    this.props.rejectWinner();
+    this.props.chooseWinner();
+  },
+  handlePlayAgain(){
+    this.props.saveWinner();
+    this.props.chooseWinner();
+  },
   render() {
     if(this.props.winner.name) {
       return (
         <div className="current-winner">
           <h2>Current Winner</h2>
           <p>{this.props.winner ? this.props.winner.name : ''}</p>
-          <button onClick={this.props.rejectWinner}>Not Here</button>
-          <button onClick={this.props.saveWinner}>Play Again</button>
+          <button onClick={this.handleNotHere}>Not Here</button>
+          <button onClick={this.handlePlayAgain}>Play Again</button>
           <button onClick={this.props.saveWinner}>Finished</button>
         </div>
       );
