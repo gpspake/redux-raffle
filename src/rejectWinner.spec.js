a90bdff5a3acfc5cd790a589015f899a825384ce
George Spake <gpspake@gmail.com>
2017-04-03 12:26:28 -0500

    create reject winner test

---
+++ src/rejectWinner.spec.js
@@ -0,0 +1,35 @@
+import { rejectWinner } from './redux/actionCreators'
+import rootReducer from './redux/reducers'
+import deepFreeze from 'deep-freeze';
+
+it('rejects the current winner', () => {
+
+  const initialState = {
+    "contestants": [
+      {"id": "111", "name": "john"},
+      {"id": "222", "name": "mary"}
+    ],
+    "winner": {"id": "333", "name": "dave"},
+    "winners": [
+      {"id": "444", "name": "joan"},
+      {"id": "555", "name": "jake"}
+    ]
+  };
+
+  deepFreeze(initialState);
+
+  const newState = {
+    "contestants": [
+      {"id": "111", "name": "john"},
+      {"id": "222", "name": "mary"},
+      {"id": "333", "name": "dave"}
+    ],
+    "winner": {},
+    "winners": [
+      {"id": "444", "name": "joan"},
+      {"id": "555", "name": "jake"}
+    ]
+  };
+
+  expect( rootReducer(initialState, rejectWinner()) ).toEqual( newState );
+});
