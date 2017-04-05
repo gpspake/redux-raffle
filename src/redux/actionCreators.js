2c6cabd2a237d0f56dd2da487dc42faf6461ad0c
George Spake <gpspake@gmail.com>
2017-04-05 17:27:05 -0500

    add fetch contestants action creators

--- src/redux/actionCreators.js
+++ src/redux/actionCreators.js
@@ -1,4 +1,5 @@
-import { CHOOSE_WINNER, SAVE_WINNER, REJECT_WINNER, UPDATE_CONTESTANTS } from './actions'
+import { CHOOSE_WINNER, SAVE_WINNER, REJECT_WINNER, UPDATE_CONTESTANTS, FETCH_CONTESTANTS,FETCH_CONTESTANTS_SUCCESS, FETCH_CONTESTANTS_FAILURE } from './actions';
+import axios from 'axios';
 
 export const chooseWinner = (winnerIndex) => {
   return { type: CHOOSE_WINNER, winnerIndex }
@@ -14,4 +15,21 @@ export const rejectWinner = () => {
 
 export const updateContestants = (newContestants) => {
   return { type: UPDATE_CONTESTANTS, newContestants }
+};
+
+export const fetchContestants = () => {
+  const request = axios.get('https://gist.githubusercontent.com/gpspake/4576702f5e3533045c53ebc15c205572/raw/3c1e54e3bd08aeda9f2b232b2db483ba5e7414b7/contestants.json');
+  console.log(request);
+  return {
+    type: FETCH_CONTESTANTS,
+    newContestants: request
+  }
+};
+
+export const fetchContestantsSuccess = (newContestants) => {
+  return { type: FETCH_CONTESTANTS_SUCCESS, newContestants }
+};
+
+export const fetchContestantsFailure = () => {
+  return { type: FETCH_CONTESTANTS_FAILURE }
 };
