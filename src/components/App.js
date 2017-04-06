bab6d9a6027d43fbf04ee3294767e0cdb5936a08
George Spake <gpspake@gmail.com>
2017-04-06 11:21:43 -0700

    remove unused import

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux'
 import store from '../redux/store'
-import chooseWinner from '../redux/actionCreators'
 import Raffle from '../containers/RaffleContainer'
 
 const App = React.createClass({
